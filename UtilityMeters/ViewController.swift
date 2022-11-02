8f7284d0206ef7bba95df795a7ea9f989ddc5728
evgen z <evgen-2208@ya.ru>
2022-11-02 20:17:55 +0400

    Initial Commit

---
+++ UtilityMeters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UtilityMeters
+//
+//  Created by Evgen on 02/11/2022.
+//  Copyright © 2022 Evgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
